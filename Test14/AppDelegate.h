d3a283028de7ee29b8530619324b864f5c2951a8
MS <3050119305@qq.com>
2018-10-17 16:35:38 +0800

    firstcommit

---
+++ Test14/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test14
+//
+//  Created by apple on 2018/10/17.
+//  Copyright © 2018年 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
